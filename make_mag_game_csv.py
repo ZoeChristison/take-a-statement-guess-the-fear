b1960077b08e51fca12888c723b78f662a2be98b
ZoeChristison <chr25019@byui.edu>
2026-03-19 20:48:11 -0600

    Made make mag game csv python file a loop so we can continue to add episodes without reruning the program every time

--- make_mag_game_csv.py
+++ make_mag_game_csv.py
@@ -8,31 +8,37 @@ def main():
     '''
     Makes CSV file to hold game info (episode number,
     episode title, episode info blurb, content warnings,
-    blurb from episode (maybe??), fear(s) associated with
+    blurb/excerpt from episode (maybe??), fear(s) associated with
     episode)
     return: None
     '''
-    # Get episode number and title
-    episode_number = input('Episode number: ')
-    episode_title = input(f'Title of episode {episode_number}: ')
+    # Make loop so we can continue to add lines to CSV
+    add_epsiode = 'y'
+    while add_epsiode == 'y':
+        # Get episode number and title
+        episode_number = input('Episode number: ')
+        episode_title = input(f'Title of episode {episode_number}: ')
 
-    # Get info that will be used as hints in game
-    episode_info_blurb = input(f'MAG {episode_number}: {episode_title} info blurb: ')
-    content_warnings = input(f'MAG {episode_number}: {episode_title} content warnings: ')
-    episode_blurb = input(f'MAG {episode_number}: {episode_title} episode blurb/key word.: ')
+        # Get info that will be used as hints in game
+        episode_info_blurb = input(f'MAG {episode_number}: {episode_title} info blurb: ')
+        content_warnings = input(f'MAG {episode_number}: {episode_title} content warnings: ')
+        episode_blurb = input(f'MAG {episode_number}: {episode_title} episode blurb/key word/excerpt: ')
 
-    # Get info on fears associated with episode
-    number_of_fears = int(input(f'How many fears are associated with MAG {episode_number}: {episode_title}? '))
-    i = 0
-    fears = []
-    while i != number_of_fears:
-        associated_fear = input('Enter fear: ')
-        fears.append(associated_fear)
-        i += 1
+        # Get info on fears associated with episode
+        number_of_fears = int(input(f'How many fears are associated with MAG {episode_number}: {episode_title}? '))
+        i = 0
+        fears = []
+        while i != number_of_fears:
+            associated_fear = input('Enter fear: ')
+            fears.append(associated_fear)
+            i += 1
 
-    # Call add_to_csv_file() to add episode info
-    add_to_csv_file(FILENAME, episode_number, episode_title, episode_info_blurb, content_warnings, episode_blurb, fears)
-    print(f'Added line: {episode_number},{episode_title},{episode_info_blurb},{content_warnings},{episode_blurb},{fears}')
+        # Call add_to_csv_file() to add episode info
+        add_to_csv_file(FILENAME, episode_number, episode_title, episode_info_blurb, content_warnings, episode_blurb, fears)
+        print(f'Added line: {episode_number},{episode_title},{episode_info_blurb},{content_warnings},{episode_blurb},{fears}')
+
+        # Continue loop
+        add_epsiode = input('Add another episode (y/n): ').lower()
 
 # Function
 # Add input to file
